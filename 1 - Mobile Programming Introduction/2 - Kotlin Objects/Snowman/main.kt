5717baa71cb4c119b062750f12a2dc8b3864b524
AsePlayer <52973701+AsePlayer@users.noreply.github.com>
2023-01-11 16:20:32 -0700

    Commented and refactored code

--- 1 - Mobile Programming Introduction/2 - Kotlin Objects/Snowman/main.kt
+++ 1 - Mobile Programming Introduction/2 - Kotlin Objects/Snowman/main.kt
@@ -3,20 +3,20 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
-
 // Make Snowman class
 class Snowman(val name: String, val age: Int, val hasTopHat: Boolean, val weight: Float) {
-    var snowmanID:Int = 0
-    var snowmanName = name
-    var snowmanAge = age
-    var snowmanHasTopHat = hasTopHat
-    var snowmanWeight = weight
+    var snowmanID = 0   // variable to store the snowman id
+    var snowmanName = name // variable to store the snowman name
+    var snowmanAge = age // variable to store the snowman age
+    var snowmanHasTopHat = hasTopHat // variable to store whether the snowman has a top hat
+    var snowmanWeight = weight // variable to store the snowman weight
 
-    // format date of birth
+    // format the current date and time and store it as the date of birth of the snowman
     val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
     val formatted = LocalDateTime.now().format(formatter)
     var dateOfBirth: String = formatted.toString()
 
+    // method to print the snowman's properties
     fun printSnowman() {
         print("\tID: $snowmanID. ")
         print("Snowman: $snowmanName. ")
@@ -26,11 +26,12 @@ class Snowman(val name: String, val age: Int, val hasTopHat: Boolean, val weight
         println("Weight: $snowmanWeight. ")
     }
 
-    // setter for ID
+    // setter method to set the snowman ID
     fun setID(newID: Int) {
         snowmanID = newID
     }
 
+    // method to update the snowman's properties
     fun updateSnowman(name: String, age: Int, hasTopHat: Boolean, weight: Float) {
         snowmanName = name
         snowmanAge = age
@@ -41,17 +42,18 @@ class Snowman(val name: String, val age: Int, val hasTopHat: Boolean, val weight
 }
 
 fun main(args: Array<String>) {
+    // create a mutable list to store the snowmen
     var snowmenList = mutableListOf<Snowman>()
-    val snowman1 = Snowman("Snowy", 5, true, 10.0f)
-    snowmenList.add(snowman1)
+    val snowman1 = Snowman("Snowy", 5, true, 10.0f) // initial snowman
+    snowmenList.add(snowman1) // add initial snowman to inventory
     
     println("Welcome to SnowManager!")
     println("This program will allow you to manage your Snowmen")
-    snowman1.printSnowman()
 
     var userChoice = 0
     var snowmanID = 0
 
+    // run the program until the user enters 9
     while(userChoice != 9) {
         println("Please choose an option")
         println("1. Create a Snowman and add it to the inventory")
@@ -63,6 +65,7 @@ fun main(args: Array<String>) {
         println("9. Quit the program")
         userChoice = Integer.parseInt(readLine())
 
+        // handle the user's choice
         when(userChoice) {
             1 -> {
                 // Add a new snowman to the inventory
@@ -75,82 +78,81 @@ fun main(args: Array<String>) {
                 println("Please enter the weight of the snowman (in KG):")
                 val weight = readLine().toString().toFloat()
 
+                // create a new snowman instance with the provided properties
                 val newSnowman = Snowman(name, age, hasTopHat, weight)
+                // set the new snowman's ID
                 newSnowman.setID(++snowmanID)
+                // add the new snowman to the list
                 snowmenList.add(newSnowman)
             }
             2 -> {
                 // Display all snowmen in the inventory
                 println("Full inventory of Snowmen:")
+                // loop through the list and print each snowman's properties
                 for (snowman in snowmenList) {
                     snowman.printSnowman()
                 }
-                println("Press ENTER to continue:")
-                readLine()
             }
             3 -> {
                 // Show a single Snowman given its ID number
                 println("Please enter the ID of the Snowman you would like to see:")
                 val inputSnowmanID = Integer.parseInt(readLine())
+                // loop through the list and find the snowman with the provided ID
                 for (snowman in snowmenList) {
                     if (snowman.snowmanID == inputSnowmanID) {
                         snowman.printSnowman()
                     }
                 }
-                println("Press ENTER to continue:")
-                readLine()
             }
             4 -> {
                 // Search for a snowman by (partial) name
-                println("Please enter full or partial name of the Snowman you would like to see:")
-                val snowmanName = readLine().toString()
+                println("Please enter a (partial) name of the Snowman you would like to search for:")
+                val inputSnowmanName = readLine().toString()
+                // loop through the list and find the snowman with the provided name
                 for (snowman in snowmenList) {
-                    if (snowman.name.contains(snowmanName)) {
+                    if (snowman.snowmanName.contains(inputSnowmanName)) {
                         snowman.printSnowman()
                     }
                 }
-                println("Press ENTER to continue:")
-                readLine()
             }
             5 -> {
-                // List Snowmen
-                for (snowman in snowmenList) {
-                    snowman.printSnowman()
-                }
-                // Delete a Snowman
-                println("Please enter the ID of the Snowman you would like to delete:")
+                // Remove a snowman from the inventory by ID
+                println("Please enter the ID of the Snowman you would like to remove:")
                 val inputSnowmanID = Integer.parseInt(readLine())
+                // loop through the list and remove the snowman with the provided ID
                 for (snowman in snowmenList) {
                     if (snowman.snowmanID == inputSnowmanID) {
                         snowmenList.remove(snowman)
-                        println("Snowman deleted!")
-                        break
                     }
                 }
             }
             6 -> {
-                // List Snowmen
-                for (snowman in snowmenList) {
-                    snowman.printSnowman()
-                }
-                // Change a Snowman
+                // Change a snowman's properties by ID
                 println("Please enter the ID of the Snowman you would like to change:")
                 val inputSnowmanID = Integer.parseInt(readLine())
+                // loop through the list and find the snowman with the provided ID
                 for (snowman in snowmenList) {
                     if (snowman.snowmanID == inputSnowmanID) {
-                        println("Please enter the name of the snowman:")
+                        println("Please enter the new name of the snowman:")
                         val name = readLine().toString()
-                        println("Please enter the age of the snowman:")
+                        println("Please enter the new age of the snowman:")
                         val age = Integer.parseInt(readLine())
-                        println("Does the snowman have a top hat? (true/false):")
+                        println("Does the snowman have a top hat now? (true/false):")
                         val hasTopHat = if(readLine().toString().contains('t')) true else false
-                        println("Please enter the weight of the snowman (in KG):")
+                        println("Please enter the new weight of the snowman (in KG):")
                         val weight = readLine().toString().toFloat()
-
+                        // update the snowman's properties
                         snowman.updateSnowman(name, age, hasTopHat, weight)
                     }
-                }  
+                }
+            }
+            9 -> {
+                // Exit the program
+                println("Exiting SnowManager, Goodbye!")
+                return
             }
         }
+        println("Press ENTER to continue:")
+        readLine()
     }
 }
--- 1 - Mobile Programming Introduction/2 - Kotlin Objects/Snowman/tempCodeRunnerFile.kt
+++ 1 - Mobile Programming Introduction/2 - Kotlin Objects/Snowman/tempCodeRunnerFile.kt
@@ -1,1 +1,1 @@
-name
+implementation
