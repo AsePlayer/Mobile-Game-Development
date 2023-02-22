1188c317e0ecbfc886a44c388b25be5451b56f4e
AsePlayer <aseplayerofficial@hotmail.com>
2023-02-21 19:07:20 -0700

    Deviated from professor on user input

--- 4 - Implicit Intents And Managing Lists/Jokes App with Data Source and Search/MainActivity.kt
+++ 4 - Implicit Intents And Managing Lists/Jokes App with Data Source and Search/MainActivity.kt
@@ -6,17 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.Divider
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -24,6 +22,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.ryan.jokesapp.data.JokeModel
+import com.ryan.jokesapp.data.inmemory.ViewModelInMemory
 import com.ryan.jokesapp.ui.theme.JokesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,64 +35,91 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
     @Composable
     fun MyApp() {
-        var jokes = remember {
-            mutableStateListOf(
-                com.ryan.jokesapp.data.JokeModel(
-                    0,
-                    "What is black and white and red all over?",
-                    "Penguin in a blender!",
-                    answerIsVisible = true
-                ),
-                com.ryan.jokesapp.data.JokeModel(
-                    1,
-                    "Knock knock",
-                    "Who's there?",
-                    answerIsVisible = false
-                ),
-                com.ryan.jokesapp.data.JokeModel(
-                    2,
-                    "Why did the tomato turn red?",
-                    "Because it saw the salad dressing!",
-                    answerIsVisible = true
-                ),
-                com.ryan.jokesapp.data.JokeModel(
-                    3,
-                    "Why were the police searching for hardened criminals?",
-                    "They stole all the Viagra!",
-                    answerIsVisible = true
-                )
+        val viewModel: ViewModelInMemory = ViewModelInMemory()
 
-            )
-        }
+        MainScreen(
+            jokesList = viewModel.jokesList,
+            jokesSearchResult = viewModel.jokesSearchResult,
+            doAddJoke = {viewModel.addJoke(it)},
+            doSearch = {viewModel.findJokesByKeyword(it)},
+            doShowHide = {viewModel.hideShowJoke(it)}
+        )
+    }
 
-        // A surface container using the 'background' color from the theme
+    @Composable
+    fun MainScreen(
+        jokesList : List<JokeModel>,
+        jokesSearchResult: List<JokeModel>,
+        doAddJoke: (JokeModel) -> Unit,
+        doSearch: (String) -> Unit,
+        doShowHide: (JokeModel) -> Unit
+    ) {
+        var jokeText by remember { mutableStateOf("") } // track the text in the input field
+        var jokePunchlineText by remember { mutableStateOf("") } // track the text in the input field
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            LazyColumn() {
-                items(jokes.size) { index ->
-                    Joke1(joke = jokes[index],
-                        {
-                            Log.d("Joke Tag", "MyApp: You clicked joke number $it")
-                            // replace the joke at position 'it' with a new joke.
-                            // replacing on element wil cause the mutableStateListOf
-                            // variable to re-render the UI elements
-                            // notice the ! character that toggles the boolean values for answerIsVisible.
-                            jokes[it] = com.ryan.jokesapp.data.JokeModel(
-                                it,
-                                jokes[it].question,
-                                jokes[it].answer,
-                                !jokes[it].answerIsVisible
+            Column() {
+                // Text input field for entering jokes
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    value = jokeText,
+                    onValueChange = { jokeText = it },
+                    label = { Text("Enter a joke") }
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    value = jokePunchlineText,
+                    onValueChange = { jokePunchlineText = it },
+                    label = { Text("Enter a punchline") }
+                )
+
+                // Button to add the joke to the list
+                Button(
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(16.dp),
+                    onClick = {
+                        val newJoke =
+                            JokeModel(
+                                jokesList.size,
+                                jokeText,
+                                jokePunchlineText,
+                                true
                             )
-                        }
-                    )
+
+                        doAddJoke(newJoke)
+                        jokeText = "" // clear the input field
+                        jokePunchlineText = "" // clear the input field
+                    }
+                ) {
+                    Text("Add Joke")
                 }
-            }
-        }
-    }
+
+                LazyColumn() {
+                    //Log.d("Main", "MainScreen: show all ${jokesList.size} jokes")
+
+                    items(jokesList.size) {index ->
+                        Joke1(
+                            joke = jokesList[index]
+                        ) {
+                            Log.d("Main","Show ${index}")
+                            doShowHide(jokesList[index])
+                        }
+                    } // items
+                } // lazy column
+            } // column
+        } // surface
+    } // main screen
+
 
     @Composable
     fun Joke1(joke: JokeModel, changeVisibility: (id: Int) -> Unit) {
@@ -121,6 +147,6 @@ class MainActivity : ComponentActivity() {
             )
         }
         Divider()
-
     }
-}
+} // main activity
+
